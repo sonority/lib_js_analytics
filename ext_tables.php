3d563a797666653b0073f5dd57beabf14689250b
stephankellermayr <stephan.kellermayr@gmail.com>
2020-03-01 15:04:30 +0100

    [TASK] Removed unused ext_tables.php

--- ext_tables.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-defined('TYPO3_MODE') or die();
-
-// Add static typoscript configurations
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'JSlibs: Google Analytics Trackingcode');
